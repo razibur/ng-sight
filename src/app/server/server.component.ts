bce9164ae3bbc0edede1ca5477b32e98111e0c9b
razibur <razibur@outlook.com>
2018-05-18 17:10:18 -0400

    Create server component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,18 @@
+import { Server } from './../shared/Server';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() serverInput: Server;
+
+  ngOnInit() {
+  }
+
+}
