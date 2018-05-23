96e3fcda5b51a754ca26f213ff5b525dbf3cfc5e
razibur <razibur@outlook.com>
2018-05-22 20:09:55 -0400

    Get pie chart data from database

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { THEME_COLORS } from './../../shared/theme.colors';
+import { Component, OnInit, Input } from '@angular/core';
+import * as _ from 'lodash';
+
+const theme = 'Bright';
 
 @Component({
   selector: 'app-pie-chart',
@@ -9,17 +13,39 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
-  pieChartData: number[] = [350, 450, 120];
+  @Input() inputData: any;
+  @Input() limit: any;
+
+  pieChartData: number[] ; // = [350, 450, 120];
   colors: any[] = [
     {
-      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
+      // backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
+      // borderColor: '#111'
+
+      backgroundColor: this.themeColors(theme),
       borderColor: '#111'
     }
   ];
-  pieChartLabels: string[] = ['XYZ Logistics', 'Main St Bakery', 'Acme Hosting'];
+  pieChartLabels: string[] ; // = ['XYZ Logistics', 'Main St Bakery', 'Acme Hosting'];
   pieChartType = 'doughnut';
 
   ngOnInit() {
+    this.parseChartData(this.inputData, this.limit);
   }
 
+  parseChartData(res: any, limit?: number) {
+    console.log('response: ', res);
+
+    const allData = res.slice(0, limit);
+    console.log('allData (slice): ', allData);
+
+    this.pieChartData = allData.map(x => _.values(x)[1]);
+    this.pieChartLabels = allData.map(x => _.values(x)[0]);
+  }
+
+  themeColors(setName: string): string[] {
+    const c = THEME_COLORS.slice(0)
+      .find(set => set.name === setName).colorSet;
+    return c;
+  }
 }
--- src/app/sections/section-sales/section-sales.component.ts
+++ src/app/sections/section-sales/section-sales.component.ts
@@ -1,3 +1,4 @@
+import { SalesDataService } from './../../services/sales-data.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionSalesComponent implements OnInit {
 
-  constructor() { }
+  salesDataByCustomer: any;
+  salesDataByState: any;
+
+  constructor(private _salesDataService: SalesDataService) { }
 
   ngOnInit() {
+    this._salesDataService.getOrdersByCustomer(5).subscribe(res => {
+      this.salesDataByCustomer = res;
+    });
+
+    this._salesDataService.getOrdersByState().subscribe(res => {
+      this.salesDataByState = res;
+    });
   }
 
 }
